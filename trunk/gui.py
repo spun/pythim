694f1ba163bf9ac51967785fdf8e941829cd6d27
victornmatrix <victornmatrix@296dbd78-8dc5-11de-927e-ab586cd9c9de>
2009-08-24 13:59:16 +0000

    Añadido borde a la caja de texto, añadido servidor.py, añadido libNotify.py, añadido Instrucciones.txt para comprender el funcionamiento.

--- trunk/gui.py
+++ trunk/gui.py
@@ -4,42 +4,37 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 
+import socket
+
 import webbrowser
 
 from about import About
 from tray_icon import TrayIcon
-
 from menu_bar import MenuBar
+from libNotify import notification
 
 import cons
 
 class Gui(gtk.Window):
 	""""""
-	# Our new improved callback.  The data passed to this method
-	# is printed to stdout.
-	def callback(self, widget, data):
-		print "Hello again - %s was pressed" % data
-
-	# another callback
-	def delete_event(self, widget, event, data=None):
-		gtk.main_quit()
-		return gtk.FALSE
-
 	def muestra_texto(self, widget, entry):
 		entry_text = entry.get_text()
 		print "Contenido caja: %s\n" % entry_text
 		#Iterador al final (donde se va a anyadir)
 		final = self.textbuffer.get_end_iter()
-		#Hacemos el salto de linea
+		#Mandamos el mensaje al servidor
+		self.s.send(entry_text)
+		#Primera version de notificaciones
+		if entry_text == "beta":
+			g = notification()
+			g.notify()
+			
+		#Hacemos el salto de linea		
 		entry_text = entry_text + "\n"
 		#Insertamos texto
 		self.textbuffer.insert(final,entry_text)
 		#Vaciamos la caja de entrada de texto
 		entry.set_text("")
-
-		
-	def menuitem_response(self, widget, string):
-		print "%s" % string
 		
 	def __init__(self):
 
@@ -53,24 +48,37 @@ class Gui(gtk.Window):
 		
 		#self.move(self.x, self.y)
 		
-		self.connect("delete_event", self.delete_event)
+		self.connect("delete_event", self.quit)
+		
+		# socket cliente
+		self.s = socket.socket()
+		self.s.connect(("localhost", 9999))
+
+		
+		
 		
 		self.vbox = gtk.VBox()
 		self.add(self.vbox)
 		self.vbox.show()	
 		
-		#menu items
+		# menu items
 		menu_quit = gtk.STOCK_QUIT, self.quit
 		menu_help = gtk.STOCK_HELP, self.help
 		menu_about = gtk.STOCK_ABOUT, About
 		menu_preferences = gtk.STOCK_PREFERENCES, self.quit
 
-		#menubar
+		# menubar
 		file_menu = ("Archivo"), [menu_quit]
 		view_menu = ("Editar"), [menu_preferences]
 		help_menu = ("Ayuda"), [menu_help, menu_about]
 		self.vbox.pack_start(MenuBar([file_menu, view_menu, help_menu]), False)
 		self.vbox.show()		
+		
+		# frame de aspecto para el textview (funciona, pero creo que debe existir otra cosa)
+		frame = gtk.Frame(None)
+		frame.set_border_width(5)
+		self.vbox.pack_start(frame, True, True, 0)
+		frame.show()		
 		
 		#textview box
 		sw = gtk.ScrolledWindow()
@@ -80,21 +88,20 @@ class Gui(gtk.Window):
 		sw.add(textview)
 		sw.show()
 		textview.show()
-		self.vbox.pack_start(sw)
-		self.textbuffer.set_text("Hola, soy el texto de prueba \n")		
-		textview.set_editable(gtk.FALSE)
+		frame.add(sw)
+		self.textbuffer.set_text("Hola, soy el texto de prueba\n")		
+		textview.set_editable(False)
 		
 		#entry box
 		entry = gtk.Entry()
 		entry.set_max_length(50)
 		entry.connect("activate", self.muestra_texto, entry)
 		entry.set_text("Tu texto")
+
 		entry.insert_text(" aqui", len(entry.get_text()))
 		entry.select_region(0, len(entry.get_text()))
-		self.vbox.pack_end(entry, gtk.TRUE, gtk.TRUE, 0)
+		self.vbox.pack_end(entry, True, True, 0)
 		entry.show()
-		
-		
 		
 		self.show()
 		
@@ -103,14 +110,18 @@ class Gui(gtk.Window):
 		self.tray_icon = TrayIcon(self.show, self.hide, tray_menu)
 		
 		
+
+		
 	def help(self, widget):
 		""""""
 		webbrowser.open(cons.DOC)
 		
 	def quit(self, dialog=None, response=None):
 		""""""
+		self.s.send("quit")
+		self.s.close()
 		gtk.main_quit()
-		return gtk.FALSE
+		return False
 
 def main():
 	gtk.main()
---
+++ trunk/servidor.py
@@ -0,0 +1,16 @@
+import socket
+
+s = socket.socket()
+s.bind(("localhost", 9999))
+s.listen(1)
+sc, addr = s.accept()
+
+while True:
+	recibido = sc.recv(1024)
+	if recibido == "quit":
+		break
+	print "Recibido:", recibido
+	sc.send(recibido)
+print "adios"
+sc.close()
+s.close()
