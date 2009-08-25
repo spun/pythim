26d6b8f9a087e5a9a7c8b3fb56bade497b3b1243
victornmatrix <victornmatrix@296dbd78-8dc5-11de-927e-ab586cd9c9de>
2009-08-25 01:01:08 +0000

    Version separada de Cliente/Servidor con sockets incompletos

--- trunk/Server/servidor.py
+++ trunk/Server/servidor.py
@@ -1,16 +1,41 @@
-import socket
+#! /usr/bin/env python
 
-s = socket.socket()
-s.bind(("localhost", 9999))
-s.listen(1)
-sc, addr = s.accept()
+import __builtin__
+import os.path
+import sys
+import logging
 
-while True:
-	recibido = sc.recv(1024)
-	if recibido == "quit":
-		break
-	print "Recibido:", recibido
-	sc.send(recibido)
-print "adios"
-sc.close()
-s.close()
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gobject
+
+from gui import Gui
+
+class Server:
+	""""""
+	def __init__(self):
+		""""""
+		#exception hook
+		self.old_exception_hook = sys.excepthook
+		sys.excepthook = self.exception_hook
+
+	def exception_hook(self, type, value, trace):
+		""""""
+		file_name = trace.tb_frame.f_code.co_filename
+		line_no = trace.tb_lineno
+		exception = type.__name__
+		self.logger.critical("File %s line %i - %s: %s" % (file_name, line_no, exception, value))
+		print self.old_exception_hook(type, value, trace)
+		self.exit(-1)
+
+	def exit(self, arg=0):
+		""""""
+		self.logger.debug("Exit: %s" % arg)
+		exit(arg)
+
+if __name__ == "__main__":
+	gobject.threads_init()
+	p = Server()
+	Gui()
+	gtk.main()
